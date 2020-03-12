eef81baa4f6af8f0baca181754684477746f7da8
HALIN004 <61605960+HALIN004@users.noreply.github.com>
2020-03-12 18:19:32 -0500

    Update db.php

--- src/config/db.php
+++ src/config/db.php
@@ -2,9 +2,9 @@
     class db{
         // Properties
         private $dbhost = 'remotemysql.com';
-        private $dbuser = 'y64MtsQZL5';
-        private $dbpass = 'ASbmFQRbdB';
-        private $dbname = 'y64MtsQZL5';
+        private $dbuser = 'hl3JhPXXde';
+        private $dbpass = 'T6vZSRUphf';
+        private $dbname = 'hl3JhPXXde';
 
         // Connect
         public function connect(){
